4be0bf096df49b2f880c105288d4a06ef6fc1a09
odahub <oda.dahlen@gmail.com>
2018-07-02 23:33:12 +0200

    Refactored mainactivity

--- DuErNormal/app/src/main/java/com/example/oda/duernormal/MainActivity.java
+++ DuErNormal/app/src/main/java/com/example/oda/duernormal/MainActivity.java
@@ -5,9 +5,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,21 +16,42 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         // The buttons
-        Button mBkropp;
-        Button mButseendet;
-        Button mBsosialt;
-        Button mBforeldre;
-        Button mBpersonlighet;
-        Button mBgenerelt;
-        Button mBtilfeldige;
+        Button mBkropp = findViewById(R.id.mBkropp);
+        Button mButseendet = findViewById(R.id.mButseendet);
+        Button mBsosialt = findViewById(R.id.mBsosialt);
+        Button mBforeldre = findViewById(R.id.mBforeldre);
+        Button mBpersonlighet = findViewById(R.id.mBpersonlighet);
+        Button mBgenerelt = findViewById(R.id.mBgenerelt);
+        Button mBtilfeldige = findViewById(R.id.mBtilfeldige);
 
-        mBkropp = (Button) findViewById(R.id.mBkropp);
-        mBkropp.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
+        mBkropp.setOnClickListener(this);
+        mButseendet.setOnClickListener(this);
+        mBsosialt.setOnClickListener(this);
+        mBforeldre.setOnClickListener(this);
+        mBpersonlighet.setOnClickListener(this);
+        mBgenerelt.setOnClickListener(this);
+        mBtilfeldige.setOnClickListener(this);
+
+
+        //mBkropp.setOnClickListener(new View.OnClickListener() {
+           // @Override
+            //public void onClick(View view) {
+            //    startActivity(new Intent(MainActivity.this, DisplayKropp.class));
+            //}
+        //});
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch(v.getId()) {
+            case R.id.mBkropp:
                 startActivity(new Intent(MainActivity.this, DisplayKropp.class));
-            }
-        });
+                break;
+            case R.id.mButseendet:
+                startActivity(new Intent(MainActivity.this, DisplayKropp.class));
+                break;
+        }
 
     }
 }
---
+++ DuErNormal/app/src/test/java/com/example/oda/duernormal/MainActivityTest.java
@@ -0,0 +1,7 @@
+package com.example.oda.duernormal;
+
+import static org.junit.Assert.*;
+
+public class MainActivityTest {
+
+}
